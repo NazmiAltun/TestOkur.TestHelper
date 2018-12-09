11de53d2fe287eac6cfa3291c7aebbe7439d76ae
NAZMI <NAZMI@DESKTOP-20STISF>
2018-12-10 01:05:44 +0800

    AntiForgeryToken extract method implemented

--- src/TestOkur.TestHelper/Extensions/HttpResponseMessageExtensions.cs
+++ src/TestOkur.TestHelper/Extensions/HttpResponseMessageExtensions.cs
@@ -1,13 +1,27 @@
-﻿namespace FluentAssertions
+﻿namespace TestOkur.TestHelper.Extensions
 {
     using System.Net.Http;
-    using Primitives;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+    using FluentAssertions.Primitives;
 
     public static class HttpResponseMessageExtensions
     {
         public static HttpResponseMessageAssertions Should(this HttpResponseMessage instance)
         {
             return new HttpResponseMessageAssertions(instance);
+        }
+
+        public static async Task<string> ExtractAntiForgeryToken(this HttpResponseMessage response)
+        {
+            var responseAsString = await response.Content.ReadAsStringAsync();
+            return await Task.FromResult(ExtractAntiForgeryToken(responseAsString));
+        }
+
+        private static string ExtractAntiForgeryToken(string htmlResponseText)
+        {
+            var match = Regex.Match(htmlResponseText, @"\<input name=""__RequestVerificationToken"" type=""hidden"" value=""([^""]+)"" \/\>");
+            return match.Success ? match.Groups[1].Captures[0].Value : null;
         }
     }
 }
