bd7f90133527bf9ed7cb80f192fcf0f62d64c49c
NAZMI <NAZMI@DESKTOP-20STISF>
2018-12-10 01:36:30 +0800

    CreatePostRequestWithCookies method implemented

--- src/TestOkur.TestHelper/Extensions/HttpResponseMessageExtensions.cs
+++ src/TestOkur.TestHelper/Extensions/HttpResponseMessageExtensions.cs
@@ -1,8 +1,11 @@
 ﻿namespace TestOkur.TestHelper.Extensions
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Text.RegularExpressions;
     using System.Threading.Tasks;
+    using Microsoft.Net.Http.Headers;
 
     public static class HttpResponseMessageExtensions
     {
@@ -17,10 +20,44 @@
             return await Task.FromResult(ExtractAntiForgeryToken(responseAsString));
         }
 
+        public static HttpRequestMessage CreatePostRequestWithCookies(
+            this HttpResponseMessage response,
+            string path,
+            Dictionary<string, string> formPostBodyData)
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, path)
+            {
+                Content = new FormUrlEncodedContent(formPostBodyData.ToList())
+            };
+            var cookies = response.ExtractCookiesFromResponse();
+
+            cookies.Keys.ToList().ForEach(key =>
+            {
+                httpRequestMessage.Headers.Add("Cookie", new CookieHeaderValue(key, cookies[key]).ToString());
+            });
+
+            return httpRequestMessage;
+        }
+
         private static string ExtractAntiForgeryToken(string htmlResponseText)
         {
             var match = Regex.Match(htmlResponseText, @"\<input name=""__RequestVerificationToken"" type=""hidden"" value=""([^""]+)"" \/\>");
             return match.Success ? match.Groups[1].Captures[0].Value : null;
+        }
+
+        private static IDictionary<string, string> ExtractCookiesFromResponse(this HttpResponseMessage response)
+        {
+            var result = new Dictionary<string, string>();
+
+            if (response.Headers.TryGetValues("Set-Cookie", out var values))
+            {
+                SetCookieHeaderValue.ParseList(values.ToList()).ToList().ForEach(cookie =>
+                {
+                    result.Add(cookie.Name.Value, cookie.Value.Value);
+                });
+            }
+
+            return result;
         }
     }
 }
