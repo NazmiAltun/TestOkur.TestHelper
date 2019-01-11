ff91ca45b58b69a24e3383765aee8725a8caa373
NAZMI <NAZMI@DESKTOP-20STISF>
2019-01-12 02:30:06 +0800

    TestAuthenticationOptions shouldn't be abstract.

--- src/TestOkur.TestHelper/TestAuthenticationOptions.cs
+++ src/TestOkur.TestHelper/TestAuthenticationOptions.cs
@@ -3,8 +3,8 @@
     using Microsoft.AspNetCore.Authentication;
     using System.Security.Claims;
 
-    public abstract class TestAuthenticationOptions : AuthenticationSchemeOptions
+    public class TestAuthenticationOptions : AuthenticationSchemeOptions
     {
-        public abstract ClaimsIdentity Identity { get; }
+        public virtual ClaimsIdentity Identity { get; }
     }
 }
