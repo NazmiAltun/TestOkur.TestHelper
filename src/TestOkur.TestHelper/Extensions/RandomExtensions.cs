9aadac7a54f913c5ddc59b3f4d406fc3df671571
NAZMI <NAZMI@DESKTOP-20STISF>
2019-02-08 10:09:11 +0300

    RandomHelper method moved to RandomExtensions

---
+++ src/TestOkur.TestHelper/Extensions/RandomExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TestOkur.TestHelper.Extensions
+{
+	using System;
+	using System.Linq;
+
+	public static class RandomExtensions
+	{
+		public static string RandomString(this Random random, int length)
+		{
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+			return new string(Enumerable.Repeat(chars, length)
+				.Select(s => s[random.Next(s.Length)]).ToArray());
+		}
+	}
+}
