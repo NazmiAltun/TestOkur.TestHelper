fd56ba2af02299d7c19a9bb968176b956632e482
NAZMI <NAZMI@DESKTOP-20STISF>
2019-05-23 13:32:13 +0300

    Thread-safe random class added

---
+++ src/TestOkur.TestHelper/RandomGen.cs
@@ -0,0 +1,27 @@
+﻿namespace TestOkur.TestHelper
+{
+	using System;
+	using System.Threading;
+	using TestOkur.TestHelper.Extensions;
+
+	public static class RandomGen
+	{
+		private static Random _global = new Random();
+		private static ThreadLocal<Random> _local = new ThreadLocal<Random>(() =>
+		{
+			int seed;
+			lock (_global)
+			{
+				seed = _global.Next();
+			}
+
+			return new Random(seed);
+		});
+
+		public static int Next(int maxValue) => _local.Value.Next(maxValue);
+
+		public static int Next() => _local.Value.Next();
+
+		public static string String(int len) => _local.Value.RandomString(len);
+	}
+}
