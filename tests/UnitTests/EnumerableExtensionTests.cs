c3c5d55df8c4fc99004ff76b930e07680df9270f
NAZMI <NAZMI@DESKTOP-20STISF>
2019-07-05 17:11:31 +0300

    a few simple tests added

---
+++ tests/UnitTests/EnumerableExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace UnitTests
+{
+	using System.Collections.Generic;
+	using FluentAssertions;
+	using TestOkur.TestHelper.Extensions;
+	using Xunit;
+
+	public class EnumerableExtensionTests
+	{
+		[Fact]
+		public void RandomShouldReturnElementFromTheList()
+		{
+			var list = new List<int> {67, 34, 87, 12, 5, 57};
+			list.Contains(list.Random()).Should().BeTrue();
+		}
+	}
+}
---
+++ tests/UnitTests/RandomGenTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using TestOkur.TestHelper;
+using Xunit;
+
+namespace UnitTests
+{
+	public class RandomGenTests
+	{
+		[Fact]
+		public void RandomString_Should_GenerateString_WithProvidedLength()
+		{
+			RandomGen.String(10).Should().HaveLength(10);
+		}
+	}
+}
